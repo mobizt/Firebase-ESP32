c70aebd56d76ecde96917beb4a8f0321bcbb3809
suwatchai <k_suwatchai@hotmail.com>
2019-12-24 22:16:50 +0700

    Update example

--- examples/AutomaticPlantWatering/AutomaticPlantWatering.ino
+++ examples/AutomaticPlantWatering/AutomaticPlantWatering.ino
@@ -19,6 +19,19 @@
   Two pumps will be set to turn on in the moring and evening for 120 second everyday
   To manually turn on and off both pumps, change the value under /PlantWatering/control
 
+  To control the device, send command at /PlantWatering/control/cmd and the result from process
+  showed at /PlantWatering/status/terminal
+
+  The command and its description.
+
+  idle: nothing to do
+  get-time: get time from NTP server
+  boot: restart the device
+  load-pump: load pump configuration
+  load-schedule: load schedule configuration
+  pump-count: show the number of pumps at /PlantWatering/status/terminal
+  schedule-count: show the number of schedules at /PlantWatering/status/terminal
+
 */
 
 //FirebaseESP8266.h must be included before ESP8266WiFi.h
@@ -56,42 +69,30 @@ FirebaseData firebaseData2;
 
 String path = "/PlantWatering";
 bool timeReady = false;
+//Change to match your time zone
+float time_zone = 3;
+float daylight_offset_in_sec = 0;
 
-//For 4 presets
-preset_time_t presetTime[4];
+//For 10 presets
+preset_time_t presetTime[10];
 
-//For 2 pumps
-pump_info_t pumpInfo[2];
-
+//For 5 pumps
+pump_info_t pumpInfo[5];
 
 void setPumpState(int pumpIndex, int state);
 void streamCallback(StreamData data);
 void streamTimeoutCallback(bool timeout);
 void setDeviceTime(float time_zone, float daylight_offset_in_sec);
-void setDailyTimer(int presetIndex, int pumpIndex, int activeState, int inactiveState, int hour, int min, int sec, int duration_in_sec);
-void runTimer();
+void setDailySchedule(int presetIndex, int pumpIndex, int activeState, int inactiveState, int hour, int min, int sec, int duration_in_sec, FirebaseJsonArray *scheduleConfig);
+void runSchedule();
+void loadSchedule(FirebaseData &data);
+void setPump(int pumpIndex, String id, int gpio, int state, FirebaseJsonArray *pumpConfig);
+void loadPump(FirebaseData &data);
 
 void setup()
 {
 
     Serial.begin(115200);
-
-    //Setup initial pump data
-    //state = 0 for Off, state = 1 for On
-    //id should not contains space or special chars
-
-    pumpInfo[0].gpio = 16;
-    pumpInfo[0].id = "Pump1";
-    pumpInfo[0].state = 0;
-
-    pumpInfo[1].gpio = 17;
-    pumpInfo[1].id = "Pump2";
-    pumpInfo[1].state = 0;
-
-    //Set output
-
-    pinMode(pumpInfo[0].gpio, OUTPUT);
-    pinMode(pumpInfo[1].gpio, OUTPUT);
 
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting to Wi-Fi");
@@ -107,10 +108,6 @@ void setup()
 
     //get time from NTP server and set up
     Serial.println("Get time from NTP server...");
-    
-    //Change to match your time zone
-    float time_zone = 3;
-    float daylight_offset_in_sec = 0;
     setDeviceTime(time_zone, daylight_offset_in_sec);
 
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
@@ -121,32 +118,69 @@ void setup()
 
     Firebase.setStreamCallback(firebaseData1, streamCallback, streamTimeoutCallback);
 
+    Firebase.set(firebaseData2, path + "/control/cmd", "idle");
+
+    if (!Firebase.pathExist(firebaseData2, path + "/config/pump"))
+    {
+        //Setup initial pump data
+
+        FirebaseJsonArray pumpConfig;
+
+        //pump index, id, gpio, state, pump config
+        setPump(0, "Pump1", 16, 0, &pumpConfig);
+        setPump(1, "Pump2", 12, 0, &pumpConfig);
+
+        Firebase.set(firebaseData2, path + "/config/pump", pumpConfig);
+    }
+    else
+    {
+        if (Firebase.get(firebaseData2, path + "/config/pump"))
+            loadPump(firebaseData2);
+    }
+
     //Set up node for pump if not exist.
     int pumpNum = sizeof(pumpInfo) / sizeof(pumpInfo[0]);
     for (int i = 0; i < pumpNum; i++)
     {
-        if (!Firebase.pathExist(firebaseData2, path + "/control/" + pumpInfo[i].id))
+        if (pumpInfo[i].id != "")
         {
-            Firebase.set(firebaseData2, path + "/control/" + pumpInfo[i].id, pumpInfo[i].state);
-            Firebase.set(firebaseData2, path + "/status/" + pumpInfo[i].id, pumpInfo[i].state);
+            if (!Firebase.pathExist(firebaseData2, path + "/control/" + pumpInfo[i].id))
+            {
+                Firebase.set(firebaseData2, path + "/control/" + pumpInfo[i].id, pumpInfo[i].state);
+                Firebase.set(firebaseData2, path + "/status/" + pumpInfo[i].id, pumpInfo[i].state);
+            }
         }
     }
 
     if (timeReady)
     {
-        Serial.println("Setup timer...");
 
-        //preset index, pump index, activeState, inactiveState, hour, minute, second, duration in second
+        if (!Firebase.pathExist(firebaseData2, path + "/config/schedule"))
+        {
 
-        //Set for Pump1 (index 0) to turn on (1) 120 seconds from 7:30:00 then turn off (0)
-        setDailyTimer(0, 0, 1, 0, 7, 30, 00, 120);
-        //Set for Pump1 (index 0) to turn on (1) 120 seconds from 17:30:00 then turn off (0)
-        setDailyTimer(1, 0, 1, 0, 17, 30, 00, 120);
+            Serial.println("Setup timer...");
 
-        //Set for Pump2 (index 1) to turn on (1) 120 seconds from 7:30:00 then turn off (0)
-        setDailyTimer(2, 1, 1, 0, 7, 30, 00, 120);
-        //Set for Pump2 (index 1) to turn on (1) 120 seconds from 17:30:00 then turn off (0)
-        setDailyTimer(3, 1, 1, 0, 17, 30, 00, 120);
+            FirebaseJsonArray scheduleConfig;
+
+            //preset index, pump index, activeState, inactiveState, hour, minute, second, duration in second, new setting
+
+            //Set for Pump1 (index 0) to turn on (1) 120 seconds from 7:30:00 then turn off (0)
+            setDailySchedule(0, 0, 1, 0, 7, 30, 00, 120, &scheduleConfig);
+            //Set for Pump1 (index 0) to turn on (1) 120 seconds from 17:30:00 then turn off (0)
+            setDailySchedule(1, 0, 1, 0, 17, 30, 00, 120, &scheduleConfig);
+
+            //Set for Pump2 (index 1) to turn on (1) 120 seconds from 7:30:00 then turn off (0)
+            setDailySchedule(2, 1, 1, 0, 7, 30, 00, 120, &scheduleConfig);
+            //Set for Pump2 (index 1) to turn on (1) 120 seconds from 17:30:00 then turn off (0)
+            setDailySchedule(3, 1, 1, 0, 17, 30, 00, 120, &scheduleConfig);
+
+            Firebase.set(firebaseData2, path + "/config/schedule", scheduleConfig);
+        }
+        else
+        {
+            if (Firebase.get(firebaseData2, path + "/config/schedule"))
+                loadSchedule(firebaseData2);
+        }
     }
 
     Serial.println("Ready!");
@@ -154,7 +188,7 @@ void setup()
 
 void loop()
 {
-    runTimer();
+    runSchedule();
 }
 
 void setPumpState(int pumpIndex, int state)
@@ -198,7 +232,64 @@ void streamCallback(StreamData data)
         for (int i = 0; i < pumpNum; i++)
         {
             if (data.dataPath() == "/" + pumpInfo[i].id)
+            {
                 setPumpState(i, data.intData());
+                String status = pumpInfo[i].id;
+                if (data.intData() == 0)
+                    status += " Off";
+                else if (data.intData() == 1)
+                    status += " On";
+                Firebase.set(firebaseData2, path + "/status/terminal", status);
+            }
+        }
+    }
+    else if (data.dataPath() == "/cmd")
+    {
+        if (data.stringData() == "get-time")
+        {
+            Serial.println("cmd: get time from NTP server");
+            Firebase.set(firebaseData2, path + "/status/terminal", "get time");
+            setDeviceTime(time_zone, daylight_offset_in_sec);
+        }
+        else if (data.stringData() == "load-pump")
+        {
+            Serial.println("cmd: load pump");
+            Firebase.set(firebaseData2, path + "/status/terminal", "load pump");
+            if (Firebase.get(firebaseData2, path + "/config/pump"))
+                loadPump(firebaseData2);
+        }
+        else if (data.stringData() == "load-schedule")
+        {
+            Serial.println("cmd: load schedule");
+            Firebase.set(firebaseData2, path + "/status/terminal", "load schedule");
+            if (Firebase.get(firebaseData2, path + "/config/schedule"))
+                loadSchedule(firebaseData2);
+        }
+        if (data.stringData() == "schedule-count")
+        {
+            int presetNum = sizeof(presetTime) / sizeof(presetTime[0]);
+            Serial.println("cmd: schedule-count");
+            int count = 0;
+            for (int i = 0; i < presetNum; i++)
+                if (presetTime[i].state > 0)
+                    count++;
+            Firebase.set(firebaseData2, path + "/status/terminal", String(count));
+        }
+        if (data.stringData() == "pump-count")
+        {
+            int pumpNum = sizeof(pumpInfo) / sizeof(pumpInfo[0]);
+            Serial.println("cmd: pump-count");
+            int count = 0;
+            for (int i = 0; i < pumpNum; i++)
+                if (pumpInfo[i].id != "")
+                    count++;
+            Firebase.set(firebaseData2, path + "/status/terminal", String(count));
+        }
+        else if (data.stringData() == "boot")
+        {
+            Serial.println("cmd: reboot device");
+            Firebase.set(firebaseData2, path + "/status/terminal", "restart device");
+            ESP.restart();
         }
     }
 }
@@ -225,9 +316,36 @@ void setDeviceTime(float time_zone, float daylight_offset_in_sec)
     }
 
     timeReady = now > 8 * 3600 * 2;
+    if (timeReady)
+        Firebase.set(firebaseData2, path + "/status/terminal", "idle");
+    else
+        Firebase.set(firebaseData2, path + "/status/terminal", "cannot get time");
 }
 
-void setDailyTimer(int presetIndex, int pumpIndex, int activeState, int inactiveState, int hour, int min, int sec, int duration_in_sec)
+void setPump(int pumpIndex, String id, int gpio, int state, FirebaseJsonArray *pumpConfig)
+{
+    int pumpNum = sizeof(pumpInfo) / sizeof(pumpInfo[0]);
+
+    if (pumpIndex < pumpNum)
+    {
+        pumpInfo[pumpIndex].id = id;
+        pumpInfo[pumpIndex].gpio = gpio;
+        pumpInfo[pumpIndex].state = state;
+        pinMode(gpio, OUTPUT);
+
+        if (pumpConfig)
+        {
+            FirebaseJson json;
+            json.add("id", id);
+            json.add("gpio", gpio);
+            json.add("state", state);
+            json.add("idx", pumpIndex);
+            pumpConfig->add(json);
+        }
+    }
+}
+
+void setDailySchedule(int presetIndex, int pumpIndex, int activeState, int inactiveState, int hour, int min, int sec, int duration_in_sec, FirebaseJsonArray *scheduleConfig)
 {
     int presetNum = sizeof(presetTime) / sizeof(presetTime[0]);
 
@@ -241,10 +359,23 @@ void setDailyTimer(int presetIndex, int pumpIndex, int activeState, int inactive
         presetTime[presetIndex].pump_index = pumpIndex;
         presetTime[presetIndex].active = activeState;
         presetTime[presetIndex].inactive = inactiveState;
+
+        if (scheduleConfig)
+        {
+            FirebaseJson json;
+            json.add("hour", hour);
+            json.add("min", min);
+            json.add("sec", sec);
+            json.add("dur", duration_in_sec);
+            json.add("idx", pumpIndex);
+            json.add("act", activeState);
+            json.add("ina", inactiveState);
+            scheduleConfig->add(json);
+        }
     }
 }
 
-void runTimer()
+void runSchedule()
 {
     struct tm current_timeinfo;
     struct tm target_timeinfo;
@@ -275,27 +406,26 @@ void runTimer()
                 {
                     if (presetTime[i].active != pumpInfo[presetTime[i].pump_index].state)
                     {
-                        Serial.print(pumpInfo[presetTime[i].pump_index].id + " will be activated for ");
-                        Serial.print(presetTime[i].duration);
-                        Serial.print(" second");
-                        if (presetTime[i].duration > 1)
-                            Serial.print("s");
-                        Serial.print(" from ");
+                        String status = pumpInfo[presetTime[i].pump_index].id + " activated - " + String(presetTime[i].duration) + " sec from ";
+
                         if (target_timeinfo.tm_hour < 10)
-                            Serial.print("0");
-                        Serial.print(target_timeinfo.tm_hour);
-                        Serial.print(":");
+                            status += "0";
+                        status + String(target_timeinfo.tm_hour);
+                        status += ":";
                         if (target_timeinfo.tm_min < 10)
-                            Serial.print("0");
-                        Serial.print(target_timeinfo.tm_min);
-                        Serial.print(":");
+                            status + "0";
+                        status + String(target_timeinfo.tm_min);
+                        status += ":";
                         if (target_timeinfo.tm_sec < 10)
-                            Serial.print("0");
-                        Serial.println(target_timeinfo.tm_sec);
+                            status += "0";
+                        status + String(target_timeinfo.tm_sec);
+
+                        Serial.println(status);
 
                         presetTime[i].state = 2;
                         Firebase.set(firebaseData2, path + "/control/" + pumpInfo[presetTime[i].pump_index].id, presetTime[i].active);
                         setPumpState(presetTime[i].pump_index, presetTime[i].active);
+                        Firebase.set(firebaseData2, path + "/status/terminal", status);
                     }
                 }
             }
@@ -309,9 +439,69 @@ void runTimer()
                         presetTime[i].state = 1;
                         Firebase.set(firebaseData2, path + "/control/" + pumpInfo[presetTime[i].pump_index].id, presetTime[i].inactive);
                         setPumpState(presetTime[i].pump_index, presetTime[i].inactive);
+                        String status = pumpInfo[presetTime[i].pump_index].id + " deactivated";
+                        Firebase.set(firebaseData2, path + "/status/terminal", status);
                     }
                 }
             }
         }
+    }
+}
+
+void loadSchedule(FirebaseData &data)
+{
+
+    FirebaseJsonArray *scheduleConfig = data.jsonArrayPtr();
+    FirebaseJson json;
+
+    Serial.println("Schedule config:");
+
+    for (size_t i = 0; i < scheduleConfig->size(); i++)
+    {
+        FirebaseJsonData &jsonData = data.jsonData();
+        scheduleConfig->get(jsonData, i);
+        Serial.println(jsonData.stringValue);
+        json.setJsonData(jsonData.stringValue);
+        json.get(jsonData, "hour");
+        int hour = jsonData.intValue;
+        json.get(jsonData, "min");
+        int min = jsonData.intValue;
+        json.get(jsonData, "sec");
+        int sec = jsonData.intValue;
+        json.get(jsonData, "dur");
+        int dur = jsonData.intValue;
+        json.get(jsonData, "idx");
+        int idx = jsonData.intValue;
+        json.get(jsonData, "act");
+        int act = jsonData.intValue;
+        json.get(jsonData, "ina");
+        int ina = jsonData.intValue;
+        setDailySchedule(i, idx, act, ina, hour, min, sec, dur, nullptr);
+    }
+}
+
+void loadPump(FirebaseData &data)
+{
+
+    FirebaseJsonArray *scheduleConfig = data.jsonArrayPtr();
+    FirebaseJson json;
+
+    Serial.println("Pump config:");
+
+    for (size_t i = 0; i < scheduleConfig->size(); i++)
+    {
+        FirebaseJsonData &jsonData = data.jsonData();
+        scheduleConfig->get(jsonData, i);
+        Serial.println(jsonData.stringValue);
+        json.setJsonData(jsonData.stringValue);
+        json.get(jsonData, "id");
+        String id = jsonData.stringValue;
+        json.get(jsonData, "state");
+        int state = jsonData.intValue;
+        json.get(jsonData, "gpio");
+        int gpio = jsonData.intValue;
+        json.get(jsonData, "idx");
+        int index = jsonData.intValue;
+        setPump(index, id, gpio, state, nullptr);
     }
 }
