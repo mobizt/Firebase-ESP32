fc0d6290b61682e0883e2ebdd433bb89a5f13d7e
suwatchai <k_suwatchai@hotmail.com>
2022-04-15 10:25:40 +0700

    Fixed external Client (ESP8266 core v2.x.x) compilation error.

--- src/FirebaseESP32.h
+++ src/FirebaseESP32.h
@@ -1,14 +1,14 @@
 #ifndef FIREBASE_CLIENT_VERSION
-#define FIREBASE_CLIENT_VERSION "3.15.5"
+#define FIREBASE_CLIENT_VERSION "3.15.6"
 #endif
 
 /**
- * Google's Firebase Realtime Database Arduino Library for ESP32, v3.15.5
+ * Google's Firebase Realtime Database Arduino Library for ESP32, v3.15.6
  *
- * Created March 5, 2022
+ * Created April 15, 2022
  *
  *   Updates:
- * - Fixed compile error for FirebaseJsonArray remove.
+ * - Fixed external Client (ESP8266 core v2.x.x) compilation error.
  *
  *
  * This library provides ESP32 to perform REST API by GET PUT, POST, PATCH,
